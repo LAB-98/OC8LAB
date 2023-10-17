dfab4a6e68e949e6a51ca4f1cf90d45b0cf42444
LAB LAB <you@example.com>
2023-10-17 18:23:03 +0200

    corrections fautes et modificatiin du contenu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,112 +1,112 @@
 // 1. Menu Toggle
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close');
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 if(navToggle){
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    });
+	navToggle.addEventListener('click', () =>{
+		navMenu.classList.add('show-menu')
+	});
 }
 
 /*===== MENU HIDDEN =====*/
 if(navClose){
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    });
+	navClose.addEventListener('click', () =>{
+		navMenu.classList.remove('show-menu')
+	});
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link');
 
 const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show-menu');
+	const navMenu = document.getElementById('nav-menu');
+	navMenu.classList.remove('show-menu');
 };
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
 // 2. Swiper Configurations
 /*=============== SWIPER PROJECTS ===============*/
 let swiperProjects = new Swiper(".projects__container", {
-    loop: true,
-    spaceBetween: 24,
-    
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    breakpoints: {
-        1200: {
-          slidesPerView: 2,
-          spaceBetween: -56,
-        },
-    },
+	loop: true,
+	spaceBetween: 24,
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	pagination: {
+		el: ".swiper-pagination",
+	},
+	breakpoints: {
+		1200: {
+			slidesPerView: 2,
+			spaceBetween: -56,
+		},
+	},
 });
 
 /*=============== SWIPER TESTIMONIAL ===============*/
 let swiperTestimonial = new Swiper(".testimonial__container", {
-    grabCursor: true,
-    
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+	grabCursor: true,
+
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
 });
 
 // 3. Email JS Configuration
 /*=============== EMAIL JS ===============*/
 const contactForm = document.getElementById('contact-form'),
-      contactName = document.getElementById('contact-name'),
-      contactEmail = document.getElementById('contact-email'),
-      contactProject = document.getElementById('contact-project'),
-      contactMessage = document.getElementById('contact-message');
+contactName = document.getElementById('contact-name'),
+contactEmail = document.getElementById('contact-email'),
+contactProject = document.getElementById('contact-project'),
+contactMessage = document.getElementById('contact-message');
 
 // EmailJS Init
 emailjs.init('qv_e5vmPYX2hEJMnr');
 
 const sendEmail = (e) => {
-    e.preventDefault();
+	e.preventDefault();
 
-    if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
-        contactMessage.classList.remove('color-blue');
-        contactMessage.classList.add('color-red');
-        contactMessage.textContent = 'Write all the input fields 📩';
-    } else {
-        emailjs.sendForm('default_service', 'template_v7mn56e', '#contact-form')
-            .then(() => {
-                contactMessage.classList.add('color-blue');
-                contactMessage.textContent = 'Message sent ✅';
-                setTimeout(() => {
-                    contactMessage.textContent = '';
-                }, 5000);
-            }, (error) => {
-                alert('OOPS! SOMETHING HAS FAILED...', error);
-            });
+	if (contactName.value === '' || contactEmail.value === '' || contactProject.value === '') {
+		contactMessage.classList.remove('color-blue');
+		contactMessage.classList.add('color-red');
+		contactMessage.textContent = 'Write all the input fields 📩';
+	} else {
+		emailjs.sendForm('service_syda36i', 'template_v7mn56e', '#contact-form')
+		.then(() => {
+			contactMessage.classList.add('color-blue');
+			contactMessage.textContent = 'Message sent ✅';
+			setTimeout(() => {
+				contactMessage.textContent = '';
+			}, 5000);
+		}, (error) => {
+			alert('OOPS! SOMETHING HAS FAILED...', error);
+		});
 
-        contactName.value = '';
-        contactEmail.value = '';
-        contactProject.value = '';
-    }
+		contactName.value = '';
+		contactEmail.value = '';
+		contactProject.value = '';
+	}
 };
 contactForm.addEventListener('submit', sendEmail);
 
 // 4. Scroll Handling
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]');
-    
+
 const scrollActive = () =>{
-  	const scrollY = window.pageYOffset;
+	const scrollY = window.pageYOffset;
 
 	sections.forEach(current =>{
 		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+		sectionTop = current.offsetTop - 58,
+		sectionId = current.getAttribute('id'),
+		sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
 
 		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
 			sectionsClass.classList.add('active-link');
@@ -126,8 +126,8 @@ window.addEventListener('scroll', scrollUp);
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () =>{
-  const header = document.getElementById('header');
-  this.scrollY >= 50 ? header.classList.add('bg-header') : header.classList.remove('bg-header');
+	const header = document.getElementById('header');
+	this.scrollY >= 50 ? header.classList.add('bg-header') : header.classList.remove('bg-header');
 };
 window.addEventListener('scroll', scrollHeader);
 
@@ -144,24 +144,24 @@ const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dar
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
 
 if (selectedTheme) {
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+	document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+	themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
 }
 
 themeButton.addEventListener('click', () => {
-    document.body.classList.toggle(darkTheme);
-    themeButton.classList.toggle(iconTheme);
-    localStorage.setItem('selected-theme', getCurrentTheme());
-    localStorage.setItem('selected-icon', getCurrentIcon());
+	document.body.classList.toggle(darkTheme);
+	themeButton.classList.toggle(iconTheme);
+	localStorage.setItem('selected-theme', getCurrentTheme());
+	localStorage.setItem('selected-icon', getCurrentIcon());
 });
 
 // 6. Animations
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-  origin: 'top',
-  distance: '60px',
-  duration: 2500,
-  delay: 400,
+	origin: 'top',
+	distance: '60px',
+	duration: 2500,
+	delay: 400,
 });
 
 sr.reveal(`.home__data, .projects__container, .testimonial__container, .footer__container`);
